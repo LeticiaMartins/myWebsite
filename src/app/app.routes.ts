ccfa7cbeb01788d7c51fa9bd7dcd4c2d45801a47
LeticiaMartins <leticia.pascale@aluno.ufabc.edu.br>
2024-05-20 22:06:52 -0300

    alterando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Component Pages
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    // { path: 'about', component: AboutComponent },
+    // { path: 'contact', component: ContactComponent },
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
